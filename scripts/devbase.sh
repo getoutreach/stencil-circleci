f2e1f3c88256c700a5f3e23c3008f25fd6e16f95
Jared Allard <jared.allard@outreach.io>
2022-04-07 13:39:38 -0700

    update stencil-base

--- scripts/devbase.sh
+++ scripts/devbase.sh
@@ -8,27 +8,27 @@ lockfile="$DIR/../bootstrap.lock"
 
 # get_field_from_yaml reads a field from a yaml file using either go-yq or python-yq
 get_field_from_yaml() {
-	field="$1"
-	file="$2"
+  field="$1"
+  file="$2"
 
-	if [[ "$(yq e '.a' '-' <<<'{"a": "true"}' 2>&1)" == "true" ]]; then
-		# using golang version
-		yq e "$field" "$file"
-	else
-		# probably using python version
-		yq -r "$field" <"$file"
-	fi
+  if [[ "$(yq e '.a' '-' <<<'{"a": "true"}' 2>&1)" == "true" ]]; then
+    # using golang version
+    yq e "$field" "$file"
+  else
+    # probably using python version
+    yq -r "$field" <"$file"
+  fi
 }
 
 version=$(get_field_from_yaml .versions.devbase "$lockfile")
 existingVersion=$(cat "$libDir/.version" 2>/dev/null || true)
 
 if [[ ! -e $libDir ]] || [[ $existingVersion != "$version" ]] || [[ ! -e "$libDir/.version" ]]; then
-	rm -rf "$libDir" || true
+  rm -rf "$libDir" || true
 
-	git clone -q --single-branch --branch "$version" git@github.com:getoutreach/devbase "$libDir" >/dev/null
-	echo -n "$version" >"$libDir/.version"
+  git clone -q --single-branch --branch "$version" git@github.com:getoutreach/devbase "$libDir" >/dev/null
+  echo -n "$version" >"$libDir/.version"
 
-	# Don't let devbase be confused by the existence of one there :(
-	rm "$libDir/service.yaml"
+  # Don't let devbase be confused by the existence of one there :(
+  rm "$libDir/service.yaml"
 fi
