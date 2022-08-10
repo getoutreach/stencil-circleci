76fe31798ad14b184f95dbba09c9b6f2dfdf51e6
artemchernyak-outreach <artem.chernyak@outreach.io>
2022-08-10 10:16:08 -0500

    feat: enable rc branch release flow

---
+++ templates/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/getoutreach/stencil/pkg/stenciltest"
+)
+
+// Replace this with your own tests.
+func TestRenderAFile(t *testing.T) {
+	st := stenciltest.New(t, "your_file.ext.tpl", "helpers.tpl")
+	st.Args(map[string]interface{}{
+		"argument": true,
+	})
+	st.Run(false)
+}
