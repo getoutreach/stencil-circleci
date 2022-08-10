843083ec12ad74c8d1348e8eb85216d556e3c281
Artem Chernyak <104374862+artemchernyak-outreach@users.noreply.github.com>
2022-08-10 13:21:47 -0500

    feat: enable rc branch release flow (#26)
    * feat: enable rc branch release flow
    
    * fix tests

---
+++ templates/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/getoutreach/stencil/pkg/stenciltest"
+)
+
+// Replace this with your own tests.
+func TestRenderAFile(t *testing.T) {
+	st := stenciltest.New(t, ".circleci/config.yml.tpl", "_helpers.tpl")
+	st.Run(false)
+}
