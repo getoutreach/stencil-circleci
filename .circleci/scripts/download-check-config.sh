bd9db5aa1b0cf2f544182165391e05c5bc37d994
Ashvin Ranjan <ranjanashvin@gmail.com>
2022-08-18 12:55:38 -0700

    fix: use variable for path

--- .circleci/scripts/download-check-config.sh
+++ .circleci/scripts/download-check-config.sh
@@ -9,15 +9,16 @@ if ! command -v check_config >/dev/null 2>&1; then
 
   pushd "$tempDir" >/dev/null || exit 1
   REPO=getoutreach/stork
+  BIN_LOC=/usr/local/bin/check_config
   TAG=$(gh release -R "$REPO" list | grep Latest | awk '{ print $1 }')
   echo "Using check_config version: ($TAG)"
   gh release -R "$REPO" download "$TAG" --pattern "stork_*_$(go env GOOS)_$(go env GOARCH).tar.gz"
 
   echo "" # Fixes issues with output being corrupted in CI
   tar xf stork**.tar.gz
-  sudo mv check_config /usr/local/bin/check_config
-  sudo chown circleci:circleci /usr/local/bin/check_config
-  sudo chmod +x /usr/local/bin/check_config
+  sudo mv check_config $BIN_LOC
+  sudo chown circleci:circleci $BIN_LOC
+  sudo chmod +x $BIN_LOC
   popd >/dev/null || exit 1
 
   rm -rf "$tempDir"
