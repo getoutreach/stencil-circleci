e070dc947ee4bf91a1b0dfae6e8643ef094b5cd2
Yujie Yan <yujie.yan@outreach.io>
2024-05-24 14:58:52 -0700

    add test case for mutual options

--- templates/main_test.go
+++ templates/main_test.go
@@ -41,6 +41,18 @@ func TestConfigForDisabledPrerelease(t *testing.T) {
 	st.Run(stenciltest.RegenerateSnapshots())
 }
 
+func TestConfigForDisabledPrereleaseWithAutoPrerelease(t *testing.T) {
+	st := stenciltest.New(t, ".circleci/config.yml.tpl", "_helpers.tpl")
+	st.Args(map[string]interface{}{
+		"releaseOptions": map[string]interface{}{
+			"enablePrereleases": false,
+			// The autoPrereleases should be ignored.
+			"autoPrereleases": true,
+		},
+	})
+	st.Run(stenciltest.RegenerateSnapshots())
+}
+
 func TestConfigForLibraryWithNodeJSGRPCClient(t *testing.T) {
 	st := stenciltest.New(t, ".circleci/config.yml.tpl", "_helpers.tpl")
 	st.Args(map[string]interface{}{
